404d063e49ca8564a38db1090b23d088bf68f348
Sijn <sijn82@gmail.com>
2018-03-12 13:21:45 +0000

    changed url in config to heroku address

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://sijn82-laravel.herokuapp.com'),
 
     /*
     |--------------------------------------------------------------------------
@@ -121,8 +121,8 @@ return [
     |
     */
 
-    'log' => env('APP_LOG', 'single'),
-
+//    'log' => env('APP_LOG', 'single'),
+    'log' => env('APP_LOG', 'errorlog'),
     'log_level' => env('APP_LOG_LEVEL', 'debug'),
 
     /*
