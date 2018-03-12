b17e9539febf55fc9408c484de75a05197969979
Sijn <sijn82@gmail.com>
2018-03-12 13:26:47 +0000

    changed url in provider to force https if !== local

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,14 +6,19 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+
     /**
      * Bootstrap any application services.
      *
      * @return void
      */
-    public function boot()
+
+    public function boot(UrlGenerator $url)
     {
-        //
+        if(env('APP_ENV') !== 'local')
+        {
+            $url->forceSchema('https');
+        }
     }
 
     /**
@@ -25,4 +30,6 @@ class AppServiceProvider extends ServiceProvider
     {
         //
     }
+
+
 }
