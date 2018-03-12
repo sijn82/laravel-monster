7275825977587918c438ace89cd4424df9e1eab6
Sijn <sijn82@gmail.com>
2018-03-12 17:57:51 +0000

    load balancing changes as suggested when using laravel/heroku pt 2

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -28,11 +28,11 @@ class TrustProxies extends Middleware
 //        Request::HEADER_X_FORWARDED_PROTO => 'X_FORWARDED_PROTO',
 //    ];
     protected $headers = [
-        Illuminate\Http\Request::HEADER_FORWARDED    => null, // not set on AWS or Heroku
-        Illuminate\Http\Request::HEADER_CLIENT_IP    => 'X_FORWARDED_FOR',
-        Illuminate\Http\Request::HEADER_CLIENT_HOST  => null, // not set on AWS or Heroku
-        Illuminate\Http\Request::HEADER_CLIENT_PROTO => 'X_FORWARDED_PROTO',
-        Illuminate\Http\Request::HEADER_CLIENT_PORT  => 'X_FORWARDED_PORT',
+        Request::HEADER_FORWARDED    => null, // not set on AWS or Heroku
+        Request::HEADER_CLIENT_IP    => 'X_FORWARDED_FOR',
+        Request::HEADER_CLIENT_HOST  => null, // not set on AWS or Heroku
+        Request::HEADER_CLIENT_PROTO => 'X_FORWARDED_PROTO',
+        Request::HEADER_CLIENT_PORT  => 'X_FORWARDED_PORT',
     ];
 
 
