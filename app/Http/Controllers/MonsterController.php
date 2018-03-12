cca7b80e0e0cf021343e41984bade74e4e09b3e7
Sijn <sijn82@gmail.com>
2018-03-12 17:28:01 +0000

    hardcoded public path

--- app/Http/Controllers/MonsterController.php
+++ app/Http/Controllers/MonsterController.php
@@ -55,7 +55,7 @@ class MonsterController extends Controller
 
         $filename = request('name').'.'.$extension;
         $monster_image_name = str_replace(' ', '_', $filename);
-        $path = public_path('img').'/'.$monster_image_name;
+        $path = 'public/img'.'/'.$monster_image_name;
 
         file_put_contents($path, $decoded);
 
